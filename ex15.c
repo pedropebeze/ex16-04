3d33486c301fc3848408357a6ec2642c3e5f96e7
pedropebeze <pedropassareli.b@gmail.com>
2025-04-16 20:28:05 -0300

    Add files via upload

---
+++ ex15.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <igualdade.h>
+
+int main(){
+
+    int a,b;
+    puts("Digite o primeiro numero:");
+    scanf("%d", &a);
+    puts("Digite o segundo numero:");
+    scanf("%d", &b);
+    if()
+    printf("%d\n",menor(a,b));
+
+    return 0;
+}
---
+++ igualdade.h
@@ -0,0 +1,24 @@
+int menor(int x,int y){
+    
+    if(x<y){
+        return x;
+    }else{
+        return y;
+    }
+}
+int maior(int x,int y){
+    
+    if(x>y){
+        return x;
+    }else{
+        return y;
+    }
+}
+int igual(int x,int y){
+    
+    if(x==y){
+        return 1;
+    }else{
+        return 0;
+    }
+}

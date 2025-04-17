332855afc9b1944697b98654dc18ecf8596aee3b
pedropebeze <pedropassareli.b@gmail.com>
2025-04-16 21:09:15 -0300

    Update ex15.c

--- ex15.c
+++ ex15.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <igualdade.h>
 
+
 int main(){
 
-    int a,b;
+    int a,b,igual;
     puts("Digite o primeiro numero:");
     scanf("%d", &a);
     puts("Digite o segundo numero:");
     scanf("%d", &b);
+    igual
     if()
     printf("%d\n",menor(a,b));
 
